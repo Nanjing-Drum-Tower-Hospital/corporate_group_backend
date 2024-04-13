d10a60538711f958d927e6b6542e47dd412ce6df
rouic <baoyage@gmail.cpom>
2024-04-13 22:16:31 +0800

    Initial commit

---
+++ src/main/java/com/njglyy/corporate_group_backend/CorporateGroupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.njglyy.corporate_group_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateGroupBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorporateGroupBackendApplication.class, args);
+    }
+
+}
