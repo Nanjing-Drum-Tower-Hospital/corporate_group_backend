7a69c7ee740bd25831119ea0d2f2526c22608a24
rouic <baoyage@gmail.cpom>
2024-08-04 22:57:36 +0800

    statement

--- src/main/java/com/njglyy/corporate_group_backend/controller/StatementController.java
+++ src/main/java/com/njglyy/corporate_group_backend/controller/StatementController.java
@@ -1,9 +1,7 @@
 package com.njglyy.corporate_group_backend.controller;
 
 import com.njglyy.corporate_group_backend.entity.*;
-import com.njglyy.corporate_group_backend.mapper.InboundMapper;
-import com.njglyy.corporate_group_backend.mapper.ManufacturerMapper;
-import com.njglyy.corporate_group_backend.mapper.OutboundMapper;
+import com.njglyy.corporate_group_backend.mapper.*;
 import org.apache.commons.io.IOUtils;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -16,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -32,12 +31,16 @@ public class StatementController {
     @Autowired
     private OutboundMapper outboundMapper;
     @Autowired
+    private ItemMapper itemMapper;
+    @Autowired
     private ManufacturerMapper manufacturerMapper;
+    @Autowired
+    private CheckOutMapper checkOutMapper;
+
     @RequestMapping(value = "/exportInboundStatement", method = RequestMethod.POST)
     public Result exportInboundStatement
             (@RequestBody Inbound inbound
             ) {
-        System.out.println(inbound);
         Inbound inboundDB = inboundMapper.queryInboundByInboundNo(inbound.getInboundNo());
 
         // Create a workbook and sheet
@@ -45,7 +48,7 @@ public class StatementController {
         Sheet sheet = workbook.createSheet("采购入库单");
 
         // Create headers
-        String[] headers = { "产品编码","产品名称","单位","数量","单价","税额", "金额","含税单价", "价税合计","税率"};
+        String[] headers = {"产品编码", "产品名称", "单位", "数量", "单价", "税额", "金额", "含税单价", "价税合计", "税率"};
         Row headerRow = sheet.createRow(0);
         for (int i = 0; i < headers.length; i++) {
             Cell cell = headerRow.createCell(i);
@@ -83,7 +86,7 @@ public class StatementController {
         }
 
         // Define the file path for saving the Excel file locally
-        String filePath = inboundDB.getInboundNo()+"采购入库单.xlsx";  // Update with your desired file path
+        String filePath = inboundDB.getInboundNo() + "采购入库单.xlsx";  // Update with your desired file path
 
         // Write the workbook to a file
         try (FileOutputStream fileOut = new FileOutputStream(filePath)) {
@@ -117,15 +120,12 @@ public class StatementController {
         }
 
 
-
-
     }
 
     @RequestMapping(value = "/exportOutboundStatement", method = RequestMethod.POST)
     public Result exportOutboundStatement
             (@RequestBody Outbound outbound
             ) {
-        System.out.println(outbound);
         Outbound outboundDB = outboundMapper.queryOutboundByOutboundNo(outbound.getOutboundNo());
 
         // Create a workbook and sheet
@@ -133,7 +133,7 @@ public class StatementController {
         Sheet sheet = workbook.createSheet("销售出库单");
 
         // Create headers
-        String[] headers = { "产品编码","产品名称","单位","数量","单价","税额", "金额","含税单价", "价税合计","税率"};
+        String[] headers = {"产品编码", "产品名称", "单位", "数量", "单价", "税额", "金额", "含税单价", "价税合计", "税率"};
         Row headerRow = sheet.createRow(0);
         for (int i = 0; i < headers.length; i++) {
             Cell cell = headerRow.createCell(i);
@@ -171,7 +171,7 @@ public class StatementController {
         }
 
         // Define the file path for saving the Excel file locally
-        String filePath = outboundDB.getOutboundNo()+"销售出库单.xlsx";  // Update with your desired file path
+        String filePath = outboundDB.getOutboundNo() + "销售出库单.xlsx";  // Update with your desired file path
 
         // Write the workbook to a file
         try (FileOutputStream fileOut = new FileOutputStream(filePath)) {
@@ -205,31 +205,43 @@ public class StatementController {
         }
 
 
-
-
     }
-
 
 
     @RequestMapping(value = "/inventoryManagementStatement", method = RequestMethod.GET)
     public Result inventoryManagementStatement
-            (             @RequestParam(value = "beginDate", required = false) LocalDate beginDate,
-                          @RequestParam(value = "endDate", required = false) LocalDate endDate
+            (@RequestParam(value = "beginDate", required = false) LocalDate beginDate,
+             @RequestParam(value = "endDate", required = false) LocalDate endDate
             ) {
-        if(beginDate==null || endDate==null){
+        if (beginDate == null || endDate == null) {
             return new Result(400, "日期不能为空！", null);
         }
-        if(beginDate.isAfter(endDate)){
+        if (beginDate.isAfter(endDate)) {
             return new Result(400, "开始日期不能晚于结束日期！", null);
         }
+        List<CheckOut> checkOutInboundList = checkOutMapper.queryCheckOutList("inbound",0, Integer.MAX_VALUE);
+        List<CheckOut> checkOutOutboundList = checkOutMapper.queryCheckOutList("outbound",0, Integer.MAX_VALUE);
 
+        boolean isNotInsideAnyInboundCheckOut = checkOutInboundList.stream()
+                .noneMatch(checkOut ->
+                        !beginDate.isBefore(checkOut.getCheckOutBeginDate()) &&
+                                !endDate.isAfter(checkOut.getCheckOutEndDate())
+                );
 
+        boolean isNotInsideAnyOutboundCheckOut = checkOutOutboundList.stream()
+                .noneMatch(checkOut ->
+                        !beginDate.isBefore(checkOut.getCheckOutBeginDate()) &&
+                                !endDate.isAfter(checkOut.getCheckOutEndDate())
+                );
+        if (isNotInsideAnyInboundCheckOut || isNotInsideAnyOutboundCheckOut) {
+            return new Result(400, "该时间区间内有未结算的账单！", null);
+        }
 
         Workbook workbook = new XSSFWorkbook();
         Sheet sheet = workbook.createSheet("收发存汇总");
 
 
-        String[] headers = { "货品名称","类型","期初数量","期初单价","金额","购入数量", "购入单价","购入金额", "发出数量","发出单价","发出金额","结存数量","结存单价","结存金额"};
+        String[] headers = {"货品名称", "类型","单位",  "期初数量", "期初单价", "金额", "购入数量", "购入单价", "购入金额", "发出数量", "发出单价", "发出金额", "结存数量", "结存单价", "结存金额"};
         Row headerRow = sheet.createRow(0);
         for (int i = 0; i < headers.length; i++) {
             Cell cell = headerRow.createCell(i);
@@ -238,40 +250,152 @@ public class StatementController {
         List<Inbound> inboundList = inboundMapper.queryInboundList(0, Integer.MAX_VALUE);
         List<Outbound> outboundList = outboundMapper.queryOutboundList(0, Integer.MAX_VALUE);
         List<Manufacturer> manufacturerList = manufacturerMapper.queryManufacturerList(0, Integer.MAX_VALUE);
+        List<Item> itemList = itemMapper.queryItemsByCondition(null,null,null,0, Integer.MAX_VALUE);
+        int rowNum = 1;
+        for (Manufacturer manufacturer : manufacturerList) {
+            BigDecimal totalInitialManufacturerPrice = new BigDecimal(0);
+            BigDecimal totalInboundManufacturerPrice = new BigDecimal(0);
+            BigDecimal totalOutboundManufacturerPrice = new BigDecimal(0);
+            BigDecimal totalFinalManufacturerPrice = new BigDecimal(0);
+            for (Item item : itemList){
+                if(item.getManufacturerId() == manufacturer.getId()){
+                    Row row = sheet.createRow(rowNum++);
+                    row.createCell(0).setCellValue(item.getName());
+                    row.createCell(1).setCellValue(item.getType());
+                    row.createCell(2).setCellValue(item.getUnitName());
+                    BigDecimal initialItemAmount = new BigDecimal(0);
+                    for(Inbound inbound : inboundList){
+                        if(inbound.getInboundDate().isBefore(beginDate)){
+                            for(InboundDetail inboundDetail : inbound.getInboundDetailList()){
+                                if(inboundDetail.getItemId() == item.getId()){
+                                    initialItemAmount = initialItemAmount.add(inboundDetail.getItemAmount()) ;
+                                }
+                            }
+                        }
+                    }
+                    for(Outbound outbound : outboundList){
+                        if(outbound.getOutboundDate().isBefore(beginDate)){
+                            for(OutboundDetail outboundDetail : outbound.getOutboundDetailList()){
+                                if(outboundDetail.getItemId() == item.getId()){
+                                    initialItemAmount = initialItemAmount.add(outboundDetail.getItemAmount().negate());
+                                }
+                            }
+                        }
+                    }
+                    row.createCell(3).setCellValue(String.valueOf(initialItemAmount));
+                    BigDecimal totalInitialItemPrice = item.getUnitPriceExcludingTax().multiply(initialItemAmount);
+                    row.createCell(4).setCellValue(item.getUnitPriceExcludingTax().setScale(10, RoundingMode.HALF_UP).toString());
+                    if (totalInitialItemPrice.compareTo(BigDecimal.ZERO) == 0) {
+                        row.createCell(5).setCellValue("0.0000000000");
+                    } else {
+                        row.createCell(5).setCellValue(totalInitialItemPrice.setScale(10, RoundingMode.HALF_UP).toString());
+                    }
+                    totalInitialManufacturerPrice=totalInitialManufacturerPrice.add(totalInitialItemPrice);
 
 
-        // Populate details
-//        int rowNum = 1;
-//        for (OutboundDetail detail : outboundDB.getOutboundDetailList()) {
-//            Row row = sheet.createRow(rowNum++);
-//            row.createCell(0).setCellValue(detail.getItem().getCode());
-//            row.createCell(1).setCellValue(detail.getItem().getName());
-//            row.createCell(2).setCellValue(detail.getItem().getUnitName());
-//            row.createCell(3).setCellValue(String.valueOf(detail.getItemAmount()));
-//            row.createCell(4).setCellValue(String.valueOf(detail.getItem().getUnitPriceExcludingTax().setScale(2, RoundingMode.HALF_UP)));
-//            row.createCell(5).setCellValue(String.valueOf(detail.getOutboundDetailTax().setScale(2, RoundingMode.HALF_UP)));
-//            row.createCell(6).setCellValue(String.valueOf(detail.getOutboundDetailPriceExcludingTax().setScale(2, RoundingMode.HALF_UP)));
-//            row.createCell(7).setCellValue(String.valueOf(detail.getItem().getUnitPriceIncludingTax().setScale(2, RoundingMode.HALF_UP)));
-//            row.createCell(8).setCellValue(String.valueOf(detail.getOutboundDetailPriceIncludingTax().setScale(2, RoundingMode.HALF_UP)));
-//            row.createCell(9).setCellValue("13%");
-//        }
-//
-//// Insert blank row
-//        sheet.createRow(rowNum++);
-//
-//// Continue with more data after the blank row
-//        Row nextRow = sheet.createRow(rowNum++);
-//        nextRow.createCell(0).setCellValue(String.valueOf("合计"));
-//        nextRow.createCell(5).setCellValue(String.valueOf(outboundDB.getOutboundTax().setScale(2, RoundingMode.HALF_UP)));
-//        nextRow.createCell(6).setCellValue(String.valueOf(outboundDB.getOutboundPriceExcludingTax().setScale(2, RoundingMode.HALF_UP)));
-//        nextRow.createCell(8).setCellValue(String.valueOf(outboundDB.getOutboundPriceIncludingTax().setScale(2, RoundingMode.HALF_UP)));
+
+                    BigDecimal inboundItemAmount = new BigDecimal(0);
+                    for(Inbound inbound : inboundList){
+                        if((inbound.getInboundDate().isAfter(beginDate) || inbound.getInboundDate().isEqual(beginDate))
+                        && (inbound.getInboundDate().isBefore(endDate) || inbound.getInboundDate().isEqual(endDate))){
+                            for(InboundDetail inboundDetail : inbound.getInboundDetailList()){
+                                if(inboundDetail.getItemId() == item.getId()){
+                                    inboundItemAmount = inboundItemAmount.add(inboundDetail.getItemAmount()) ;
+                                    System.out.println(inboundDetail.getItemAmount());
+                                }
+                            }
+                        }
+                    }
+                    row.createCell(6).setCellValue(String.valueOf(inboundItemAmount));
+                    BigDecimal totalInboundItemPrice = item.getUnitPriceExcludingTax().multiply(inboundItemAmount);
+                    row.createCell(7).setCellValue(item.getUnitPriceExcludingTax().setScale(10, RoundingMode.HALF_UP).toString());
+
+                    if (totalInboundItemPrice.compareTo(BigDecimal.ZERO) == 0) {
+                        row.createCell(8).setCellValue("0.0000000000");
+                    } else {
+                        row.createCell(8).setCellValue(totalInboundItemPrice.setScale(10, RoundingMode.HALF_UP).toString());
+                    }
+                    totalInboundManufacturerPrice=totalInboundManufacturerPrice.add(totalInboundItemPrice);
+
+
+
+
+                    BigDecimal outboundItemAmount = new BigDecimal(0);
+                    for(Outbound outbound : outboundList){
+                        if((outbound.getOutboundDate().isAfter(beginDate) || outbound.getOutboundDate().isEqual(beginDate))
+                                && (outbound.getOutboundDate().isBefore(endDate) || outbound.getOutboundDate().isEqual(endDate))){
+                            for(OutboundDetail outboundDetail : outbound.getOutboundDetailList()){
+                                if(outboundDetail.getItemId() == item.getId()){
+                                    outboundItemAmount = outboundItemAmount.add(outboundDetail.getItemAmount()) ;
+                                    System.out.println(outboundDetail.getItemAmount());
+                                }
+                            }
+                        }
+                    }
+                    row.createCell(9).setCellValue(String.valueOf(outboundItemAmount));
+                    BigDecimal totalOutboundItemPrice = item.getUnitPriceExcludingTax().multiply(outboundItemAmount);
+                    row.createCell(10).setCellValue(item.getUnitPriceExcludingTax().setScale(10, RoundingMode.HALF_UP).toString());
+
+                    if (totalOutboundItemPrice.compareTo(BigDecimal.ZERO) == 0) {
+                        row.createCell(11).setCellValue("0.0000000000");
+                    } else {
+                        row.createCell(11).setCellValue(totalOutboundItemPrice.setScale(10, RoundingMode.HALF_UP).toString());
+                    }
+                    totalOutboundManufacturerPrice=totalOutboundManufacturerPrice.add(totalOutboundItemPrice);
+
+
+                    BigDecimal finalItemAmount = new BigDecimal(0);
+                    for(Inbound inbound : inboundList){
+                        if(inbound.getInboundDate().isBefore(endDate) || inbound.getInboundDate().isEqual(endDate)){
+                            for(InboundDetail inboundDetail : inbound.getInboundDetailList()){
+                                if(inboundDetail.getItemId() == item.getId()){
+                                    finalItemAmount = finalItemAmount.add(inboundDetail.getItemAmount()) ;
+                                }
+                            }
+                        }
+                    }
+                    for(Outbound outbound : outboundList){
+                        if(outbound.getOutboundDate().isBefore(endDate) || outbound.getOutboundDate().isEqual(endDate)){
+                            for(OutboundDetail outboundDetail : outbound.getOutboundDetailList()){
+                                if(outboundDetail.getItemId() == item.getId()){
+                                    finalItemAmount = finalItemAmount.add(outboundDetail.getItemAmount().negate());
+                                }
+                            }
+                        }
+                    }
+                    row.createCell(12).setCellValue(String.valueOf(finalItemAmount));
+                    BigDecimal totalFinalItemPrice = item.getUnitPriceExcludingTax().multiply(finalItemAmount);
+                    row.createCell(13).setCellValue(item.getUnitPriceExcludingTax().setScale(10, RoundingMode.HALF_UP).toString());
+                    if (totalFinalItemPrice.compareTo(BigDecimal.ZERO) == 0) {
+                        row.createCell(14).setCellValue("0.0000000000");
+                    } else {
+                        row.createCell(14).setCellValue(totalFinalItemPrice.setScale(10, RoundingMode.HALF_UP).toString());
+                    }
+                    totalFinalManufacturerPrice=totalFinalManufacturerPrice.add(totalFinalItemPrice);
+                }
+            }
+
+            Row nextRow = sheet.createRow(rowNum++);
+            nextRow.createCell(0).setCellValue("小计（" + manufacturer.getManufacturerName() + "）");
+            nextRow.createCell(5).setCellValue(String.valueOf(totalInitialManufacturerPrice.setScale(10, RoundingMode.HALF_UP)));
+            nextRow.createCell(8).setCellValue(String.valueOf(totalInboundManufacturerPrice.setScale(10, RoundingMode.HALF_UP)));
+            nextRow.createCell(11).setCellValue(String.valueOf(totalOutboundManufacturerPrice.setScale(10, RoundingMode.HALF_UP)));
+            nextRow.createCell(14).setCellValue(String.valueOf(totalFinalManufacturerPrice.setScale(10, RoundingMode.HALF_UP)));
+        }
+        Row nextRow = sheet.createRow(rowNum++);
+        nextRow.createCell(0).setCellValue("合计");
+
+
+        nextRow = sheet.createRow(rowNum++);
+        nextRow.createCell(0).setCellValue("合计（保留小数点后2位)");
+
 //        // Auto size columns
-//        for (int i = 0; i < headers.length; i++) {
-//            sheet.autoSizeColumn(i);
-//        }
+        for (int i = 0; i < headers.length; i++) {
+            sheet.autoSizeColumn(i);
+        }
 
         // Define the file path for saving the Excel file locally
-        String filePath = beginDate+"至"+endDate+"收发存汇总表.xlsx";  // Update with your desired file path
+        String filePath = beginDate + "至" + endDate + "收发存汇总表.xlsx";  // Update with your desired file path
 
         // Write the workbook to a file
         try (FileOutputStream fileOut = new FileOutputStream(filePath)) {
@@ -303,9 +427,6 @@ public class StatementController {
             // Handle exceptions
             return new Result(400, "生成失败！", null);
         }
-
-
-
 
     }
 }
