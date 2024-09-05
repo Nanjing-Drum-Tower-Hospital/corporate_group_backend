5433a8250a0015d79341952aec5b822030ea0cbe
rouic <baoyage@gmail.cpom>
2024-09-05 08:57:56 +0800

    customer

---
+++ src/main/java/com/njglyy/corporate_group_backend/entity/Customer.java
@@ -0,0 +1,13 @@
+package com.njglyy.corporate_group_backend.entity;
+
+import java.time.LocalDate;
+
+public class Customer {
+    private int id;
+    private String name;
+    private String gender;
+    private String phoneNumber;
+    private String emailAddress;
+
+
+}
