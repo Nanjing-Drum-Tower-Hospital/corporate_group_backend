9b2a0ea14291160a8fb27a03a7b6c6f6c906739c
rouic <baoyage@gmail.cpom>
2024-09-06 09:14:18 +0800

    customer

---
+++ src/main/java/com/njglyy/corporate_group_backend/controller/CustomerController.java
@@ -0,0 +1,62 @@
+package com.njglyy.corporate_group_backend.controller;
+
+import com.njglyy.corporate_group_backend.entity.Customer;
+import com.njglyy.corporate_group_backend.entity.Result;
+import com.njglyy.corporate_group_backend.entity.Supplier;
+import com.njglyy.corporate_group_backend.mapper.CheckOutMapper;
+import com.njglyy.corporate_group_backend.mapper.CustomerMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class CustomerController {
+    @Autowired
+    private CustomerMapper customerMapper;
+    @RequestMapping(value = "/addOrUpdateCustomer", method = RequestMethod.POST)
+    public Result addOrUpdateCustomer
+            (@RequestBody Customer customer
+            ) {
+        if(customer.getName() == null){
+            return new Result(400,"客户姓名不能为空！",null);
+        }
+        else{
+            if(customer.getId()!=0){
+                customerMapper.updateCustomer(customer.getId(),customer.getName(),customer.getGender(),customer.getPhoneNumber(),customer.getEmailAddress());
+            }
+            else{
+                customerMapper.addCustomer(customer.getName(), customer.getGender(), customer.getPhoneNumber(), customer.getEmailAddress());
+            }
+
+        }
+
+        return new Result(200,"添加成功！",null);
+    }
+
+    @RequestMapping(value = "/deleteCustomerById", method = RequestMethod.GET)
+    public Result deleteCustomer
+            (@RequestParam(value = "id", required = false) int id
+            ) {
+        customerMapper.deleteCustomerById(id);
+        return new Result(200, "删除成功！", null);
+    }
+
+    @RequestMapping(value = "/queryCustomerList", method = RequestMethod.GET)
+    public Result queryCustomerList
+            (@RequestParam(value = "currentPage", required = false) int currentPage,
+             @RequestParam(value = "pageSize", required = false) int pageSize) {
+        int offset = (currentPage - 1) * pageSize;
+        List<Customer> customerList = customerMapper.queryCustomerList(offset, pageSize);
+        return new Result(200, null, customerList);
+    }
+
+    @RequestMapping(value = "/queryCustomerListCount", method = RequestMethod.GET)
+    public Result queryCustomerListCount
+            (@RequestParam(value = "currentPage", required = false) int currentPage,
+             @RequestParam(value = "pageSize", required = false) int pageSize) {
+        int customerListCount = customerMapper.queryCustomerListCount();
+        return new Result(200, null, customerListCount);
+    }
+}
---
+++ src/main/java/com/njglyy/corporate_group_backend/mapper/CustomerMapper.java
@@ -0,0 +1,127 @@
+package com.njglyy.corporate_group_backend.mapper;
+
+import com.njglyy.corporate_group_backend.entity.Customer;
+import com.njglyy.corporate_group_backend.entity.Inbound;
+import com.njglyy.corporate_group_backend.entity.InboundDetail;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Mapper
+@Repository
+public interface CustomerMapper {
+
+
+    @Insert("INSERT INTO dbo.customer_list " +
+            " values(#{name}, {gender}, #{phoneNumber}, #{emailAddress})")
+    void addCustomer(String name,String gender, String phoneNumber, String emailAddress);
+
+    @Delete("DELETE FROM customer_list " +
+            "WHERE id = #{id} ")
+    void deleteCustomerById(@Param("id") int id);
+
+    @Update("UPDATE dbo.customer_list " +
+            " set name = #{name},gender = #{gender}, phoneNumber=#{phoneNumber},emailAddress = #{emailAddress} " +
+            "where id= #{id}")
+    void updateCustomer(int id, String name, String gender, String phoneNumber, String emailAddress);
+
+
+
+    @Select("select * from customer_list " +
+            "order by id desc " +
+            "OFFSET #{offset} ROWS FETCH NEXT #{pageSize} ROWS ONLY " )
+    @Results({
+            @Result(property = "id", column = "id", id = true),
+            @Result(property = "name", column = "name"),
+            @Result(property = "gender", column = "gender"),
+            @Result(property = "phoneNumber", column = "phone_number"),
+            @Result(property = "emailAddress", column = "email_address")
+    })
+    List<Customer> queryCustomerList(int offset, int pageSize);
+
+    @Select("SELECT COUNT(*) FROM customer_list")
+    int queryCustomerListCount();
+
+//    @Select("select * from inbound_list " +
+//            "where inbound_no=#{inboundNo} " )
+//    @Results({
+//            @Result(property = "inboundNo", column = "inbound_no", id = true),
+//            @Result(property = "inboundDate", column = "inbound_date"),
+//            @Result(property = "supplierId", column = "supplier_id"),
+//            @Result(property = "remark", column = "remark"),
+//            @Result(property = "accountingReversalInboundNo", column = "accounting_reversal_inbound_no"),
+//            @Result(property = "entryType", column = "entry_type"),
+//            @Result(property = "fapiaoNo", column = "fapiao_no"),
+//            @Result(property = "supplier", column = "supplier_id",
+//                    one = @One(select = "com.njglyy.corporate_group_backend.mapper.SupplierMapper.querySupplierById")),
+//            @Result(property = "inboundDetailList", column = "inbound_no",
+//                    many = @Many(select = "queryInboundDetailListByInboundNo"))
+//    })
+//    Inbound queryInboundByInboundNo(String inboundNo);
+
+
+
+
+
+
+
+
+//
+//
+//    @Insert("INSERT INTO dbo.inbound_detail_list " +
+//            " values(#{inboundNo}, #{itemId}, #{itemAmount},#{remark})")
+//    void addInboundDetail(String inboundNo, int itemId, BigDecimal itemAmount, String remark);
+//
+//
+//    @Delete("DELETE FROM inbound_detail_list " +
+//            "WHERE inbound_no = #{inboundNo} ")
+//    void deleteInboundDetailListByInboundNo(@Param("inboundNo") String inboundNo);
+//
+//    @Delete("DELETE FROM inbound_detail_list " +
+//            "WHERE inbound_no = #{inboundNo} " +
+//            "AND item_id = #{itemId} ")
+//    void deleteInboundDetailByInboundNoAndItemId(@Param("inboundNo") String inboundNo,
+//                                                 @Param("itemId") int itemId);
+//
+//    @Update("UPDATE dbo.inbound_detail_list " +
+//            " set inbound_no = #{inboundNo},item_id=#{itemId},item_amount=#{itemAmount},remark=#{remark} " +
+//            "where id= #{id}")
+//    void updateInboundDetailById(int id,String inboundNo, int itemId, BigDecimal itemAmount,String remark);
+//
+//
+//
+//
+//    @Select("select * from inbound_detail_list where inbound_no = #{inboundNo}")
+//    @Results({
+//            @Result(property = "id", column = "id"),
+//            @Result(property = "inboundNo", column = "inbound_no"),
+//            @Result(property = "itemId", column = "item_id"),
+//            @Result(property = "itemAmount", column = "item_amount"),
+//            @Result(property = "remark", column = "remark"),
+//            @Result(property = "item", column = "item_id",
+//                    one = @One(select = "com.njglyy.corporate_group_backend.mapper.ItemMapper.queryItemById"))
+//    })
+//    List<InboundDetail> queryInboundDetailListByInboundNo(String inboundNo, int offset, int pageSize);
+//
+//    @Select("select COUNT(*) from inbound_detail_list where inbound_no = #{inboundNo}")
+//    int queryInboundDetailListCountByInboundNo(String inboundNo);
+//
+//    @Select("select * "+
+//            "from " +
+//            "inbound_detail_list \n" +
+//            "where inbound_no = #{inboundNo} and item_id = #{itemId} \n" )
+//    @Results({
+//            @Result(property = "id", column = "id"),
+//            @Result(property = "inboundNo", column = "inbound_no"),
+//            @Result(property = "itemId", column = "item_id"),
+//            @Result(property = "itemAmount", column = "item_amount"),
+//            @Result(property = "remark", column = "remark"),
+//            @Result(property = "item", column = "item_id",
+//                    one = @One(select = "com.njglyy.corporate_group_backend.mapper.ItemMapper.queryItemById"))
+//    })
+//    InboundDetail queryInboundDetailListByInboundNoAndItemId(String inboundNo, int itemId);
+
+}
